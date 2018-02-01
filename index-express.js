29d967f8121e55ec4bf24976c4d397fdceaae70f
Lucas Braun <lucasalan@gmail.com>
2018-02-01 11:30:39 +0100

    Add basic express server

---
+++ index-express.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(port, function () {
+  console.log('Server running at http://127.0.0.1:' + port);
+});
