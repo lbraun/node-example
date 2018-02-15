6a6465d33ce251732b73954fa7370df4937fdd14
Lucas Braun <lucasalan@gmail.com>
2018-02-15 10:51:57 +0100

    Add form functionality

--- angular_app/js/index.js
+++ angular_app/js/index.js
@@ -1,11 +1,11 @@
 var app = angular.module("app", []);
 
-var API_URL = "http://localhost:3000"
+var API_URL = "http://localhost:3000" + '/api/catalog/'
 
 app.controller("IndexController", ["$scope", "$http", function($scope, $http) {
   $http({
     method: 'GET',
-    url: API_URL + '/api/catalog/'
+    url: API_URL
   }).then(function successCallback(response) {
     $scope.catalog = Object.values(response.data);
     console.log($scope.catalog);
@@ -13,14 +13,57 @@ app.controller("IndexController", ["$scope", "$http", function($scope, $http) {
     // TODO: figure out how to raise a 404 in this case
   });
 
-  $scope.edit_product = function (product_id) {
-    // body...
+  $scope.new_product = function (product_index) {
+    $scope.current_product = {new: true};
+  };
+
+  $scope.update_or_create_product = function () {
+    if ($scope.current_product.new) {
+      $scope.create_product();
+    } else {
+      $scope.update_product();
+    }
+  }
+
+  $scope.create_product = function () {
+    delete $scope.current_product.new
+    var data = $scope.current_product
+
+    $http({
+      method: 'POST',
+      url: API_URL,
+      data: data
+    }).then(function successCallback(response) {
+      // Handle success
+      $scope.catalog.push(data);
+    }, function errorCallback(response) {
+      // Handle error
+    });
+  };
+
+  $scope.edit_product = function (product_index) {
+    $scope.current_product = $scope.catalog[product_index];
+  };
+
+  $scope.update_product = function () {
+    delete $scope.current_product.new
+    var data = $scope.current_product
+
+    $http({
+      method: 'PUT',
+      url: API_URL + $scope.current_product.id,
+      data: data
+    }).then(function successCallback(response) {
+      // Handle success
+    }, function errorCallback(response) {
+      // Handle error
+    });
   };
 
   $scope.delete_product = function (product_id, product_index) {
     $http({
       method: 'DELETE',
-      url: API_URL + '/api/catalog/' + product_id
+      url: API_URL + product_id
     }).then(function successCallback(response) {
       // Handle success
       $scope.catalog.splice(product_index, 1);
